dbfb66090ed39b18dbeb8ef463c92e0603c1e791
johnW_ret <retruate@gmail.com>
2022-04-30 00:36:26 -0500

    Updated example to include subjects

--- WaniKaniService.Examples/Program.cs
+++ WaniKaniService.Examples/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Text;
 using System.Reflection;
 using WaniKaniService;
 
@@ -16,31 +17,60 @@ catch (Exception)
     throw;
 }
 
+// set encoding to support Japanese characters
+Console.OutputEncoding = Encoding.UTF8;
+
 // string to response
-object response = type switch
+object response;
+
+switch (type)
 {
-    "user" => client.UserClient.GetAsync().Result,
-    _ => throw new Exception("Data type not valid.")
-};
+    case "user":
+        response = client.UserClient.GetAsync().Result;
+        break;
+
+    case "subject":
+        try
+        {
+            int id = Convert.ToInt32(args[2]);
+            response = client.SubjectsClient.GetAsync(id).Result;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        break;
+
+    default:
+        throw new Exception("Data type not valid.");
+}
 
 // write the response
+Console.WriteLine(response);
 Console.WriteLine(PropertiesToString(response));
 
 string PropertiesToString(object obj, string tab = "")
 {
+    // if it is primitive, it has no properties
+    if (obj.GetType().IsPrimitive)
+        return $"{tab}{obj.GetType().Name}: {obj?.ToString()!}";
+
     StringBuilder sb = new StringBuilder();
 
     PropertyInfo[] properties = obj.GetType().GetProperties();
     foreach (PropertyInfo pInfo in properties)
     {
-        object? p = pInfo.GetValue(obj, null);
+        object? p = pInfo.GetValue(obj);
 
         if (p is null)
             continue;
 
         sb.AppendLine($"{tab}{pInfo.Name}: {p}");
 
-        if (p.GetType().FullName!.Contains("WaniKaniService.Models"))
+        if (p is IList pIList)
+            foreach (object item in pIList)
+                sb.AppendLine(PropertiesToString(item, tab + "    "));
+        else if (p.GetType().FullName!.Contains("WaniKaniService.Models"))
             sb.AppendLine(PropertiesToString(p, tab + "    "));
     }
 
