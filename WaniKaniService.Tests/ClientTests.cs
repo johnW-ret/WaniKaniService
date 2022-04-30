b46a085b6e8bc3259e0d34f1acedfdcf7e130175
johnW_ret <retruate@gmail.com>
2022-04-30 12:23:30 -0500

    Create mock kanji test for SubjectsClient
    Created a test that tests the SubjectsClient using a mock kanji response. Wrapped all tests in a ClientTests class. Defined consts for file path and added subjects/440.json to test build folder.

---
+++ WaniKaniService.Tests/ClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaniKaniService.Tests
+{
+    public partial class ClientTests
+    {
+        /// <summary>
+        /// A fake token that matches API key regex
+        /// </summary>
+        private static readonly string fakeToken = "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa";
+
+        /// <summary>
+        /// A URI to run a server for testing
+        /// </summary>
+        private static readonly string baseUri = "http://localhost:7575/";
+
+        /// <summary>
+        /// The root response folder for test server responses
+        /// </summary>
+        private const string rootResponseFolder = "MockResponses";
+    }
+}
